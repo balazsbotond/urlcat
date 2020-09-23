55d6d5ada50e9f2bc98498f807b968d7eb40c03e
Botond Balazs <balazsbotond@gmail.com>
2020-09-23 13:12:44 +0200

    Add tests for query function

---
+++ test/query.ts
@@ -0,0 +1,36 @@
+import { query } from '../src';
+import { expect } from 'chai';
+
+describe('query', () => {
+
+  it('Returns empty string if there are no params', () => {
+    const expected = '';
+    const actual = query({});
+    expect(actual).to.equal(expected);
+  });
+
+  it('Returns single key-value pair if one param is passed', () => {
+    const expected = 'key=value';
+    const actual = query({ key: 'value' });
+    expect(actual).to.equal(expected);
+  });
+
+  it('Can handle multiple params', () => {
+    const expected = 'p1=v1&p2=v2&p3=v3';
+    const actual = query({ p1: 'v1', p2: 'v2', p3: 'v3' });
+    expect(actual).to.equal(expected);
+  });
+
+  it('Escapes the value', () => {
+    const expected = 'key=a%20%22special%22%20value';
+    const actual = query({ key: 'a "special" value' });
+    expect(actual).to.equal(expected);
+  });
+
+  it('Escapes the key', () => {
+    const expected = 'a%20%22special%22%20key=value';
+    const actual = query({ 'a "special" key': 'value' });
+    expect(actual).to.equal(expected);
+  });
+
+});
