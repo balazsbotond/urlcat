7a1da79515e56efe9f732c1f0a9ece4944be2811
Tiago Silva <tiago.toya@hotmail.com>
2020-10-01 17:59:00 -0300

    test: correcting the syntax for jest

--- test/query.ts
+++ test/query.ts
@@ -1,36 +1,35 @@
 import { query } from '../src';
-import { expect } from 'chai';
 
 describe('query', () => {
 
   it('Returns empty string if there are no params', () => {
     const expected = '';
     const actual = query({});
-    expect(actual).to.equal(expected);
+    expect(actual).toBe(expected);
   });
 
   it('Returns single key-value pair if one param is passed', () => {
     const expected = 'key=value';
     const actual = query({ key: 'value' });
-    expect(actual).to.equal(expected);
+    expect(actual).toBe(expected);
   });
 
   it('Can handle multiple params', () => {
     const expected = 'p1=v1&p2=v2&p3=v3';
     const actual = query({ p1: 'v1', p2: 'v2', p3: 'v3' });
-    expect(actual).to.equal(expected);
+    expect(actual).toBe(expected);
   });
 
   it('Escapes the value', () => {
     const expected = 'key=a+%22special%22+value';
     const actual = query({ key: 'a "special" value' });
-    expect(actual).to.equal(expected);
+    expect(actual).toBe(expected);
   });
 
   it('Escapes the key', () => {
     const expected = 'a+%22special%22+key=value';
     const actual = query({ 'a "special" key': 'value' });
-    expect(actual).to.equal(expected);
+    expect(actual).toBe(expected);
   });
 
 });
--- test/subst.ts
+++ test/subst.ts
@@ -1,72 +1,71 @@
 import { subst } from '../src';
-import { expect } from 'chai';
 
 describe('subst', () => {
 
   it('Returns empty string if the template is empty and there are no params', () => {
     const expected = '';
     const actual = subst('', {});
-    expect(actual).to.equal(expected);
+    expect(actual).toBe(expected);
   });
 
   it('Returns empty string if the template is empty but a param is passed', () => {
     const expected = '';
     const actual = subst('', { p: 1 });
-    expect(actual).to.equal(expected);
+    expect(actual).toBe(expected);
   });
 
   it('Substitutes all params present in the object passed', () => {
     const expected = '/1/a/false';
     const actual = subst('/:p/:q/:r', { p: 1, q: 'a', r: false });
-    expect(actual).to.equal(expected);
+    expect(actual).toBe(expected);
   });
 
   it('Allows parameters at the beginning of the template', () => {
     const expected = '42';
     const actual = subst(':p', { p: 42 });
-    expect(actual).to.equal(expected);
+    expect(actual).toBe(expected);
   });
 
   it('Renders boolean (true) params', () => {
     const expected = 'true';
     const actual = subst(':p', { p: true });
-    expect(actual).to.equal(expected);
+    expect(actual).toBe(expected);
   });
 
   it('Renders boolean (false) params', () => {
     const expected = 'false';
     const actual = subst(':p', { p: false });
-    expect(actual).to.equal(expected);
+    expect(actual).toBe(expected);
   });
 
   it('Renders string params', () => {
     const expected = 'test';
     const actual = subst(':p', { p: 'test' });
-    expect(actual).to.equal(expected);
+    expect(actual).toBe(expected);
   });
 
   it('Renders number params', () => {
     const expected = '234';
     const actual = subst(':p', { p: 234 });
-    expect(actual).to.equal(expected);
+    expect(actual).toBe(expected);
   });
 
   it('Throws if a param is an array', () => {
     expect(() => subst(':p', { p: [] }))
-      .to.throw(TypeError, "Path parameter p cannot be of type object. Allowed types are: boolean, string, number.");
+      .toThrowError("Path parameter p cannot be of type object. Allowed types are: boolean, string, number.");
   });
 
   it('Throws if a param is an object', () => {
     expect(() => subst(':p', { p: {} }))
-      .to.throw(TypeError, "Path parameter p cannot be of type object. Allowed types are: boolean, string, number.");
+      .toThrowError("Path parameter p cannot be of type object. Allowed types are: boolean, string, number.");
   });
 
   it('Throws if a param is a symbol', () => {
     expect(() => subst(':p', { p: Symbol() }))
-      .to.throw(TypeError, "Path parameter p cannot be of type symbol. Allowed types are: boolean, string, number.");
+      .toThrowError("Path parameter p cannot be of type symbol. Allowed types are: boolean, string, number.");
   });
 
   it('Throws if a param is missing', () => {
-    expect(() => subst(':p', {})).to.throw();
+    expect(() => subst(':p', {})).toThrow();
   });
 });
