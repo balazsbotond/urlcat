d0ee34e46b2e66176b429dbd7aff058f7de18882
Botond Balazs <balazsbotond@gmail.com>
2020-10-02 06:52:12 +0200

    Format code to follow conventions

--- src/index.ts
+++ src/index.ts
@@ -120,7 +120,9 @@ function path(template: string, params: ParamMap) {
 
   const renderedPath = template.replace(/:\w+/g, p => {
     const key = p.slice(1);
-    if ( /^\d+$/.test( key ) ) return p;
+    if (/^\d+$/.test(key)) {
+      return p;
+    }
     if (!params.hasOwnProperty(key)) {
       throw new Error(`Missing value for path parameter ${key}.`);
     }
